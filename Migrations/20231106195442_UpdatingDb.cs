9903c71ebbbd07caf28beee01fb8c95c8a29b5b8
WIUT14973 <wiut00014973@gmail.com>
2023-11-07 01:02:50 +0500

    fix: connection string updated

---
+++ Migrations/20231106195442_UpdatingDb.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DSCC.CW1._14973.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatingDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
